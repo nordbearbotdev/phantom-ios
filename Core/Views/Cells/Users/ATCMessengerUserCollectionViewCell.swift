86c3e005d8f8cbf701dbf263546639211d1dad06
nordbearbot.dev <85753549+nordbearbotdev@users.noreply.github.com>
2021-11-03 13:45:52 +0300

    Add files via upload

---
+++ Core/Views/Cells/Users/ATCMessengerUserCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ATCPhantomUserCollectionViewCell.swift
+//  Phantom
+//
+//  Created by Florian Marcu on 8/22/18.
+//  Copyright © 2018 Instamobile. All rights reserved.
+//
+
+import UIKit
+
+class ATCPhantomUserCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var nameContainerView: UIView!
+    @IBOutlet var avatarContainerView: UIView!
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var borderView: UIView!
+}
